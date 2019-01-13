33c35e0a5d11b46b036ad5a8a8cfe82d0af1b3d8
ryosukehata <hattan0523@gmail.com>
2019-01-13 20:48:10 +0900

    "add statsmode_withCI "

--- code/chap4/statsmodels_withConfidenceInterval.py
+++ code/chap4/statsmodels_withConfidenceInterval.py
@@ -11,17 +11,25 @@ df = pd.read_csv("../../RStanBook/chap04/input/data-salary.txt")
 
 
 sns.scatterplot(data=df, x=df.X, y=df.Y)
+
 result = sm.OLS(df.Y, sm.add_constant(df.X)).fit()
-_, u, b = wls_prediction_std(result)
-_, u50, b50 = wls_prediction_std(result, alpha=0.5)
 
-
-result.summary()
+result95 = result.get_prediction().summary_frame(alpha=0.05)
+result50 = result.get_prediction().summary_frame(alpha=0.5)
 
 
 sns.scatterplot(data=df, x=df.X, y=df.Y)
-plt.plot(df.X, result.predict(), 'r')
-plt.plot(df.X, u, 'r--')
-plt.plot(df.X, b, 'r--')
-plt.plot(df.X, u50, 'r-.')
-plt.plot(df.X, b50, 'r-.')
+plt.plot(df.X, result95.loc[:,"mean"], 'r')
+plt.fill_between(df.X, result95.loc[:,"mean_ci_upper"], result95.loc[:,"mean_ci_lower"], facecolor='r',alpha=0.2, label="95%")
+plt.fill_between(df.X, result50.loc[:,"mean_ci_upper"], result50.loc[:,"mean_ci_lower"], facecolor='r',alpha=0.5, label="50%")
+plt.legend()
+plt.savefig("../../output/chap4/CI.png")
+plt.show()
+
+sns.scatterplot(data=df, x=df.X, y=df.Y)
+plt.plot(df.X, result95.loc[:,"mean"], 'r')
+plt.fill_between(df.X, result95.loc[:,"obs_ci_upper"], result95.loc[:,"obs_ci_lower"], facecolor='r',alpha=0.2, label="95%")
+plt.fill_between(df.X, result50.loc[:,"obs_ci_upper"], result50.loc[:,"obs_ci_lower"], facecolor='r',alpha=0.5, label="50%")
+plt.legend()
+plt.savefig("../../output/chap4/PI.png")
+plt.show()
