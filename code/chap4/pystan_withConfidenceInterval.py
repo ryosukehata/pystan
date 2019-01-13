6ba86b65fd7755dfa492c1effae2aebedc882f1a
ryosukehata <hattan0523@gmail.com>
2019-01-13 21:08:51 +0900

    update pystan code chap4

--- code/chap4/pystan_withConfidenceInterval.py
+++ code/chap4/pystan_withConfidenceInterval.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 import numpy as np 
 import pandas as pd 
 import pystan
@@ -11,14 +8,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-# In[ ]:
-
-
 df = pd.read_csv("../../RStanBook/chap04/input/data-salary.txt"")
-
-
-# In[ ]:
-
 
 stanmodel='''
 data {
@@ -40,65 +30,17 @@ model {
 }
 '''
 
-
-# In[ ]:
-
-
 stan_input = { 'N':df.X.size, 'X':df.X, 'Y':df.Y}
-
-
-# In[ ]:
-
-
 sm = pystan.StanModel(model_code=stanmodel)
-
-
-# In[ ]:
-
-
 fit = sm.sampling(data=stan_input , iter=1000, chains=50)
-
-
-# In[ ]:
-
-
 print(fit.stansummary())
-
-
-# In[ ]:
-
-
-a_coef = fit.extract("a")["a"]
-b_coef = fit.extract("b")["b"]
-sigma_coef = fit.extract("sigma")["sigma"]
-
-
-# In[ ]:
-
 
 x=np.linspace(df.X.min(),df.X.max()+1)
 
-
-# In[ ]:
-
-
 df_stan = fit.to_dataframe()
-
-
-# In[ ]:
-
-
 df_stan=df_stan[["a","b","sigma"]]
 
-
-# In[ ]:
-
-
 sns.jointplot(data=df_stan, x="a", y="b")
-
-
-# In[ ]:
-
 
 def BayesConfidenceInterval(data, alpha=0.05):
     MeanValue = np.mean(data)
@@ -111,10 +53,6 @@ def BayesPredictionInterval(data, sigma, alpha=0.05):
     newProbabilityDistribution=np.random.normal(loc=data, scale=sigma, size=DataSize)
     MeanValue, BottomValue, UpperValue = BayesConfidenceInterval(newProbabilityDistribution, alpha=alpha)
     return MeanValue, BottomValue, UpperValue
-
-
-# In[ ]:
-
 
 mean_y=[]
 upper_CI_y=[]
@@ -147,10 +85,6 @@ for i in x:
     upper_PI50_y.append(upperPI)
     
 
-
-# In[ ]:
-
-
 sns.scatterplot(data=df, x="X", y="Y")
 plt.plot(x, mean_y, "g")
 plt.fill_between(x, upper_CI_y, bottom_CI_y, facecolor='r',alpha=0.2, label="95%")
@@ -158,20 +92,9 @@ plt.fill_between(x, upper_CI50_y, bottom_CI50_y, facecolor='r',alpha=0.4, label=
 plt.legend(loc="best")
 plt.savefig("BayesConfidenceInterval.png")
 
-
-# In[ ]:
-
-
 sns.scatterplot(data=df, x="X", y="Y")
 plt.plot(x, mean_y, "g")
 plt.fill_between(x, upper_PI_y, bottom_PI_y, facecolor='r',alpha=0.2, label="95%")
 plt.fill_between(x, upper_PI50_y, bottom_PI50_y, facecolor='r',alpha=0.4, label="50%")
 plt.legend(loc="best")
 plt.savefig("BayesPredictionInterval.png")
-
-
-# In[ ]:
-
-
-
-
